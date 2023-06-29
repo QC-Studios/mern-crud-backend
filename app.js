0ca77a5f5ffae8a5b0e9030177c578ff5c1ae8a9
sometimes-abhay <brandbuilder97@gmail.com>
2023-06-30 00:48:31 +0530

    changes

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ import {router} from './Routes/routes.js';
 const app = express();
 import './db/connection.js'
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use('/uploads', express.static('./uploads'));
 app.use('/files', express.static('./public/files'));
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
